4d2fac88b1617fe05e77215622973d01e05044f0
aman-Tomar-30 <aman.23bcon0600@jecrcu.edu.in>
2026-06-15 20:34:35 +0530

    restructure RL pipeline and remove deprecated modules

--- project/generate_csv.py
+++ project/generate_csv.py
@@ -1,23 +1,16 @@
 import csv
 import os
 
-def get_output_csv(data):
-
-    #remove /app/ to get output in local machine without using containers
-    
+def write_row_csv(row):
     os.makedirs("output", exist_ok=True)
 
-    output_file = "output/network_stats.csv"
+    output_file = "project/output/network_stats.csv"
     file_exists = os.path.isfile(output_file)
 
     with open(output_file, "a", newline="") as file:
         writer = csv.writer(file)
 
-        # Write header only if file doesn't exist
         if not file_exists:
             writer.writerow(["mac_fill", "flood_pressure", "avg_age"])
 
-        # Append rows
-        writer.writerows(data)
-
-    print("Data appended successfully!")
+        writer.writerow(row)
